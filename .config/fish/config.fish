f07b0d1c4d2fff50542f08d3d03a529c556e3345
Philipp Melab <philipp.melab@gmail.com>
2025-01-04 21:55:19 +0100

    install packages via homebrew if possible

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,11 @@ if status is-interactive
     alias cat="bat"
     alias ls="eza"
     alias glow="glow -s $HOME/.config/glow/catppuccin.json"
-
+    fish_add_path --path ~/.dotfiles/npm/node_modules/.bin/
+    fish_add_path --path ~/.dotfiles/bin/
+    fish_add_path --path /opt/homebrew/bin/
     starship init fish | source
     direnv hook fish | source
     atuin init fish | source
     set -x EDITOR hx
-    fish_add_path --path ~/.dotfiles/npm/node_modules/.bin/
-    fish_add_path --path ~/.dotfiles/bin/
-    fish_add_path --path /opt/homebrew/bin/
 end
--- nix/flake.nix
+++ nix/flake.nix
@@ -14,49 +14,65 @@
         # List packages installed in system profile. To search by name, run:
         # $ nix-env -qaP | grep wget
         environment.systemPackages = [
-          # Dotfile managment
-          pkgs.stow
-          # Prompt and history
-          pkgs.starship
-          pkgs.fish
-          pkgs.atuin
-          # Automatically load .envrc files.
-          pkgs.direnv
-          # Best editor
-          pkgs.helix
-          # File manager 
-          pkgs.yazi
-          # Git stuff
-          pkgs.lazygit
-          pkgs.git
           # Required for nix configuration in nvim
           pkgs.nixfmt-classic
           pkgs.nil
-          # Global node for global CLI tools
-          pkgs.nodejs
           pkgs.phpactor
-          # Markdown notetaking
-          pkgs.marksman
-          pkgs.obsidian
-          pkgs.glow
-          # Nice cli tools
-          pkgs.bat
-          pkgs.fd
-          pkgs.gh
-          pkgs.ripgrep
-          pkgs.httpie
-          pkgs.htop
-          pkgs.eza
-          pkgs.difftastic
         ];
         homebrew = {
           enable = true;
           onActivation = {
-            cleanup = "uninstall";
+            autoUpdate = true;
+            upgrade = true;
+            cleanup = "zap";
           };
           taps = [ "uselagoon/lagoon-cli" ];
-          brews = [ "lagoon" ];
-          casks = [ "raycast" "mitmproxy" "firefox" "font-recursive-mono-nerd-font" "1password-cli" "ghostty" ];
+          brews = [
+            # Editor
+            "helix"
+            # Dotfile managment
+            "stow"
+            # Prompt and history
+            "starship"
+            "fish"
+            "atuin"
+            # Automatically load .envrc files.
+            "direnv"
+            # File manager 
+            "yazi"
+            # Git stuff
+            "lazygit"
+            "git"
+            # Global node for global CLI tools
+            "nodejs"
+            # Markdown notetaking
+            "marksman"
+            "glow"
+            # Nice cli tools
+            "bat"
+            "fd"
+            "gh"
+            "ripgrep"
+            "httpie"
+            "htop"
+            "eza"
+            "difftastic"
+            # Lagoon CLI
+            "lagoon"
+          ];
+          casks = [
+            "font-recursive-mono-nerd-font"
+            "raycast"
+            "mitmproxy"
+            "firefox"
+            "1password-cli"
+            "1password"
+            "ghostty"
+            "obsidian"
+          ];
+          masApps = {
+            Things = 904280696;
+          };
         };
 
         # Auto upgrade nix package and the daemon service.
@@ -68,14 +84,14 @@
 
         programs.fish.enable = true;
 
-        users.users.pmelab = { shell = pkgs.fish; };
+        environment.shells = [ pkgs.fish ];
 
         # Set Git commit hash for darwin-version.
         system.configurationRevision = self.rev or self.dirtyRev or null;
 
         # Used for backwards compatibility, please read the changelog before changing.
         # $ darwin-rebuild changelog
-        system.stateVersion = 4;
+        system.stateVersion = 5;
 
         # The platform the configuration will be used on.
         nixpkgs.hostPlatform = "aarch64-darwin";
@@ -86,6 +102,45 @@
         system.defaults.dock.autohide = true;
         system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
         system.defaults.trackpad.TrackpadRightClick = true;
+        system.defaults.trackpad.Clicking = true;
+
+        system.defaults.NSGlobalDomain = {
+          # `defaults read NSGlobalDomain "xxx"`
+          "com.apple.swipescrolldirection" = true;  # enable natural scrolling(default to true)
+          "com.apple.sound.beep.feedback" = 0;  # disable beep sound when pressing volume up/down key
+          AppleInterfaceStyle = "Dark";  # dark mode
+          AppleKeyboardUIMode = 3;  # Mode 3 enables full keyboard control.
+          ApplePressAndHoldEnabled = true;  # enable press and hold
+
+          # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
+          # This is very useful for vim users, they use `hjkl` to move cursor.
+          # sets how long it takes before it starts repeating.
+          InitialKeyRepeat = 15;  # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+          # sets how fast it repeats once it starts. 
+          KeyRepeat = 3;  # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+
+          NSAutomaticCapitalizationEnabled = false;  # disable auto capitalization(自动大写)
+          NSAutomaticDashSubstitutionEnabled = false;  # disable auto dash substitution(智能破折号替换)
+          NSAutomaticPeriodSubstitutionEnabled = false;  # disable auto period substitution(智能句号替换)
+          NSAutomaticQuoteSubstitutionEnabled = false;  # disable auto quote substitution(智能引号替换)
+          NSAutomaticSpellingCorrectionEnabled = false;  # disable auto spelling correction(自动拼写检查)
+          NSNavPanelExpandedStateForSaveMode = true;  # expand save panel by default(保存文件时的路径选择/文件名输入页)
+          NSNavPanelExpandedStateForSaveMode2 = true;
+        };
+
+        system.keyboard = {
+          enableKeyMapping = true;  # enable key mapping so that we can use `option` as `control`
+
+          # NOTE: do NOT support remap capslock to both control and escape at the same time
+          remapCapsLockToControl = true;  # remap caps lock to control, useful for emac users
+          remapCapsLockToEscape  = false;   # remap caps lock to escape, useful for vim users
+
+          # swap left command and left alt 
+          # so it matches common keyboard layout: `ctrl | command | alt`
+          #
+          # disabled, caused only problems!
+          swapLeftCommandAndLeftAlt = false;  
+        };
         
         # Sudo touch id auth
         security.pam.enableSudoTouchIdAuth = true;
