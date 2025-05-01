668a3a4db041cda1a04f3bacfba6f643b7283169
Philipp Melab <philipp.melab@gmail.com>
2025-05-01 07:01:29 +0200

    WIP

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,12 @@ if status is-interactive
     alias glow="glow -s $HOME/.config/glow/catppuccin.json"
     alias pud="php -d xdebug.mode=debug vendor/bin/phpunit --filter"
     alias pu="php vendor/bin/phpunit --filter"
+    fish_add_path --path ~/.dotfiles/npm/node_modules/.bin/
     fish_add_path --path ~/.dotfiles/bin/
     fish_add_path --path /opt/homebrew/bin/
     starship init fish | source
     direnv hook fish | source
     atuin init fish | source
     zoxide init --cmd cd fish | source
-    set -x EDITOR nvim
+    set -x EDITOR hx
 end
